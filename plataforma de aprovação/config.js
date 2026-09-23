137be2cd9600a770d91eed3f150391b4ec5b3f14
giopimenta0310-jpg <giopimenta0310@gmail.com>
2026-09-22 23:22:25 -0300

    Add files via upload

---
+++ plataforma de aprovação/config.js
@@ -0,0 +1,5 @@
+// Configuração pública do Supabase (pode ficar no código: a segurança é feita pelas regras do banco)
+window.APROVACAO_CONFIG = {
+  url: "https://vbnsjwbqwldbvzuzmbbp.supabase.co",
+  key: "sb_publishable_Ag-RpOyk20hLUod8PzPnRA_FMC5U7Ox"
+};
